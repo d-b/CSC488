b102052512e214ac5c44e7cec3317aa0bc7fe921
orenwatson <g2watson@cdf.toronto.edu>
2014-01-23 11:59:28 -0800

    Added some tests. The tests of texts that are too big and integers that are too big we could decide are semantic issues.

--- A2/RUNALLTESTS.sh
+++ A2/RUNALLTESTS.sh
@@ -2,7 +2,13 @@
 
 compile() {
     INPUT=$1
-    java -jar ./compiler/dist/compiler488.jar $INPUT
+    rm ./errortmp
+    java -jar ./compiler/dist/compiler488.jar $INPUT 2>./errortmp >/dev/null
+    ERRORS=`wc -l <./errortmp`
+    if [ $ERRORS -eq 0 -a $EXPECTED -eq 0 ] ; then return 0 ; fi
+    if [ $ERRORS -ne 0  -a $EXPECTED -ne 0 ] ; then return 0 ; fi
+    echo "PROBLEM!"
+    return 1
 }
 
 runtests() {
@@ -10,9 +16,13 @@ runtests() {
     for T in tests/$1/*.488; do
         echo $T
         compile $T
+        TESTRESULT=$(($TESTRESULT + $?));
     done
 }
 
+TESTRESULT=0
+EXPECTED=0
 runtests passing
+EXPECTED=1
 runtests failing
-
+echo "Total unpassed tests:"$TESTRESULT
