7d98b919234f00cad99cb46bdcdb5fcaa1fef21b
Peter McCormick <peter@remove-me.pdmccormick.com>
2014-01-23 13:42:52 -0500

    Add a few parsing test cases and a test runner

--- A2/RUNALLTESTS.sh
+++ A2/RUNALLTESTS.sh
@@ -1,2 +1,18 @@
-#!/bin/sh
-# ...
+#!/bin/bash
+
+compile() {
+    INPUT=$1
+    java -jar ./compiler/dist/compiler488.jar $INPUT
+}
+
+runtests() {
+    SUITE=$1
+    for T in tests/$1/*.488; do
+        echo $T
+        compile $T
+    done
+}
+
+runtests passing
+runtests failing
+
