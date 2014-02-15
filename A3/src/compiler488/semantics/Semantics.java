594012247295cabbab5fd75ce81d17f2aef9275b
Daniel Bloemendal <d.bloemendal@gmail.com>
2014-02-15 04:08:41 -0500

    some initial semantic analysis code

--- A3/src/compiler488/semantics/Semantics.java
+++ A3/src/compiler488/semantics/Semantics.java
@@ -21,7 +21,6 @@ import java.util.ListIterator;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Stack;
-import java.util.Vector;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
@@ -39,152 +38,152 @@ import java.util.Vector;
  *  @author Daniel Bloemendal
  */
 public class Semantics {
-	
+    
     /** flag for tracing semantic analysis */
-	private boolean traceSemantics = false;
-	/** file sink for semantic analysis trace */
-	private String traceFile = new String();
-	private SymbolTable symbolTable;
-	public FileWriter Tracer;
-	public File f;
+    private boolean traceSemantics = false;
+    /** file sink for semantic analysis trace */
+    private String traceFile = new String();
+    private SymbolTable symbolTable;
+    public FileWriter Tracer;
+    public File f;
 
-	/** Maps for processors and actions */
-	private Map<String, Method>  mapProcessors;
-	private Map<Integer, Method> mapActions;
+    /** Maps for processors and actions */
+    private Map<String, Method>  mapProcessors;
+    private Map<Integer, Method> mapActions;
 
-	/** Analysis state */
-	private Object        analysisTop;
-	private Stack<Object> analysisStack;
-	
-	//
-	// Processor/action management 
-	//
-	
-	void populateMappings() {
-		Class<? extends Semantics> thisClass = this.getClass();
-		for(Method method : thisClass.getDeclaredMethods()) {
-			Processor procInfo = method.getAnnotation(Processor.class);
-			Action    actInfo  = method.getAnnotation(Action.class);
-			if(procInfo != null) mapProcessors.put(procInfo.target(), method);
-			if(actInfo  != null) mapActions.put(actInfo.number(), method);
-		}
-	}
-	
-	boolean invokeProcessor(Object obj) {
-		Method m = mapProcessors.get(obj.getClass().getSimpleName());
-		if(m == null) return false;
-		analysisTop = obj;
-		
-		// Invoke the processor on object
-		try {
-			m.invoke(this, obj); return true;
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			e.printStackTrace(); return false;
-		}
-	}	
-	
-	//
-	// Semantic analysis implementation
-	//
+    /** Analysis state */
+    private Object        analysisTop;
+    private Stack<Object> analysisStack;
+    
+    //
+    // Processor/action management 
+    //
+    
+    void populateMappings() {
+        Class<? extends Semantics> thisClass = this.getClass();
+        for(Method method : thisClass.getDeclaredMethods()) {
+            Processor procInfo = method.getAnnotation(Processor.class);
+            Action    actInfo  = method.getAnnotation(Action.class);
+            if(procInfo != null) mapProcessors.put(procInfo.target(), method);
+            if(actInfo  != null) mapActions.put(actInfo.number(), method);
+        }
+    }
+    
+    boolean invokeProcessor(Object obj) {
+        Method m = mapProcessors.get(obj.getClass().getSimpleName());
+        if(m == null) return false;
+        analysisTop = obj;
+        
+        // Invoke the processor on object
+        try {
+            m.invoke(this, obj); return true;
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            e.printStackTrace(); return false;
+        }
+    }   
+    
+    //
+    // Semantic analysis implementation
+    //
      
      /** SemanticAnalyzer constructor */
-	public Semantics (){
-		symbolTable = new SymbolTable();
-	}
+    public Semantics (){
+        symbolTable = new SymbolTable();
+    }
 
-	public void Initialize() {
-		mapProcessors = new HashMap<String, Method>();
-		mapActions = new HashMap<Integer, Method>();
-		analysisStack = new Stack<Object>();
-		populateMappings();
-	}
+    public void Initialize() {
+        mapProcessors = new HashMap<String, Method>();
+        mapActions = new HashMap<Integer, Method>();
+        analysisStack = new Stack<Object>();
+        populateMappings();
+    }
 
-	public void Finalize(){  
-	}
+    public void Finalize(){  
+    }
 
-	public void Analyze(Program ast) {
-		// Add the initial element to the stack
-		analysisStack.add(ast);
-		
-		// Traverse the AST
-		while(!analysisStack.empty())
-			invokeProcessor(analysisStack.pop());
-	}
-	
-	@Processor(target = "Program")
-	void processProgram(Program program) {
-		semanticAction(00);
-		processScope(program);
-		semanticAction(01);
-	}	
-	
-	@Processor(target = "Scope")
-	void processScope(Scope scope) {
-		// Add declarations and statements to the stack
-		LinkedList<Stmt> 		  stmts = scope.getStatements().getList();
-		LinkedList<Declaration>	  decls = scope.getDeclarations().getList();
-		ListIterator<Stmt> 		  si    = stmts.listIterator(stmts.size());
-		ListIterator<Declaration> di    = decls.listIterator(decls.size());
-		while(si.hasPrevious()) analysisStack.add(si.previous());
-		while(di.hasPrevious()) analysisStack.add(di.previous());
-	}
-	
-	@Processor(target = "MultiDeclarations")
-	void processMultiDeclarations(MultiDeclarations multiDeclarations) {
-	}	
-	
-	@Processor(target = "Declaration")
-	void processDeclaration(Declaration declaration) {
-	}
-	
-	@Action(number = 00)
-	void actionProgramStart(Program program) {
-	}
-	
-	@Action(number = 01)
-	void actionProgramEnd(Program program) {
-	}
-	
-	/**
-	 *  Perform one semantic analysis action
+    public void Analyze(Program ast) {
+        // Add the initial element to the stack
+        analysisStack.add(ast);
+        
+        // Traverse the AST
+        while(!analysisStack.empty())
+            invokeProcessor(analysisStack.pop());
+    }
+    
+    @Processor(target = "Program")
+    void processProgram(Program program) {
+        semanticAction(00);
+        processScope(program);
+        semanticAction(01);
+    }   
+    
+    @Processor(target = "Scope")
+    void processScope(Scope scope) {
+        // Add declarations and statements to the stack
+        LinkedList<Stmt>          stmts = scope.getStatements().getList();
+        LinkedList<Declaration>   decls = scope.getDeclarations().getList();
+        ListIterator<Stmt>        si    = stmts.listIterator(stmts.size());
+        ListIterator<Declaration> di    = decls.listIterator(decls.size());
+        while(si.hasPrevious()) analysisStack.add(si.previous());
+        while(di.hasPrevious()) analysisStack.add(di.previous());
+    }
+    
+    @Processor(target = "MultiDeclarations")
+    void processMultiDeclarations(MultiDeclarations multiDeclarations) {
+    }   
+    
+    @Processor(target = "Declaration")
+    void processDeclaration(Declaration declaration) {
+    }
+    
+    @Action(number = 00)
+    void actionProgramStart(Program program) {
+    }
+    
+    @Action(number = 01)
+    void actionProgramEnd(Program program) {
+    }
+    
+    /**
+     *  Perform one semantic analysis action
          *  @param  actionNumber  semantic analysis action number
          */
-	void semanticAction( int actionNumber ) {
-		if( traceSemantics ){
-			if(traceFile.length() > 0 ){
-		 		//output trace to the file represented by traceFile
-		 		try{
-		 			//open the file for writing and append to it
-		 			File f = new File(traceFile);
-		 		    Tracer = new FileWriter(traceFile, true);
-		 				          
-		 		    Tracer.write("Sematics: S" + actionNumber + "\n");
-		 		    //always be sure to close the file
-		 		    Tracer.close();
-		 		}
-		 		catch (IOException e) {
-		 		  System.out.println(traceFile + 
-					" could be opened/created.  It may be in use.");
-		 	  	}
-		 	}
-		 	else{
-		 		//output the trace to standard out.
-		 		System.out.println("Sematics: S" + actionNumber );
-		 	}
-		 
-		}
-		
-		Method m = mapActions.get(actionNumber);
-		if(m == null) System.out.println("Unhandled Semantic Action: S" + actionNumber );
-		else {
-			// Invoke the semantic action. 
-			try {
-				m.invoke(this, analysisTop);
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				e.printStackTrace();
-			}
-			
-			System.out.println("Semantic Action: S" + actionNumber  );
-		}
-	}
+    void semanticAction( int actionNumber ) {
+        if( traceSemantics ){
+            if(traceFile.length() > 0 ){
+                //output trace to the file represented by traceFile
+                try{
+                    //open the file for writing and append to it
+                    File f = new File(traceFile);
+                    Tracer = new FileWriter(traceFile, true);
+                                  
+                    Tracer.write("Sematics: S" + actionNumber + "\n");
+                    //always be sure to close the file
+                    Tracer.close();
+                }
+                catch (IOException e) {
+                  System.out.println(traceFile + 
+                    " could be opened/created.  It may be in use.");
+                }
+            }
+            else{
+                //output the trace to standard out.
+                System.out.println("Sematics: S" + actionNumber );
+            }
+         
+        }
+        
+        Method m = mapActions.get(actionNumber);
+        if(m == null) System.out.println("Unhandled Semantic Action: S" + actionNumber );
+        else {
+            // Invoke the semantic action. 
+            try {
+                m.invoke(this, analysisTop);
+            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            
+            System.out.println("Semantic Action: S" + actionNumber  );
+        }
+    }
 }
