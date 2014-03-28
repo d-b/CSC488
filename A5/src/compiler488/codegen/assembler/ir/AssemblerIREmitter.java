81c15ef23cace74543c619eeb3beff4edae4b03d
Daniel Bloemendal <d.bloemendal@gmail.com>
2014-03-28 09:09:21 -0400

    fixed end label offset

--- A5/src/compiler488/codegen/assembler/ir/AssemblerIREmitter.java
+++ A5/src/compiler488/codegen/assembler/ir/AssemblerIREmitter.java
@@ -21,7 +21,7 @@ public class AssemblerIREmitter extends AssemblerMachineEmitter {
         emitter.emit(Machine.PUSH, strAddr);
         emitter.emit(Machine.LOAD);
         emitter.emit(Machine.LT);
-        emitter.emit(Machine.PUSH, (short) (topLabel + 21));
+        emitter.emit(Machine.PUSH, (short) (topLabel + 20));
         emitter.emit(Machine.BF);
         emitter.emit(Machine.DUP);
         emitter.emit(Machine.PUSH, (short) (strAddr + 1));
